43b22dadce04458cdddaec46c987e3f8be844460
Keagan <sir.ellenberger@gmail.com>
2020-03-31 13:59:38 +0200

    getSeeds from a house done

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -4,8 +4,8 @@ module Oware
 type StartingPosition =
     | South
     | North
- 
-let getSeeds n board = failwith "Not implemented"
+
+let getSeeds n (board:int list) = board.Item(n-1)  //g17e4476: get's the seed's at a location on the board, n-1 as we always start from 0
 
 let useHouse n board = failwith "Not implemented"
 
@@ -18,14 +18,13 @@ let gameState board = failwith "Not implemented"
 [<EntryPoint>]
 let main _ =
     printfn "Hello from F#!"
+    let n = 12
    // 0 // return an integer exit code
-    let board = [4; 4; 4; 4; 4; 4; 4; 4; 4; 4; 4; 4]
+    (*
+   // Trying to get the getSeeds function working and it does. 
+    let board =[1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12]
+    let x = getSeeds n board
+    printfn "The  nr of seeds: %A" x
     printfn "Holes: %A" board
+    *)
     0
-(*Thoughts: 
-3 Phases: Game states ? Collecting, Sowing, Capturing <- All in a turn
-
-
-
-
-*)
