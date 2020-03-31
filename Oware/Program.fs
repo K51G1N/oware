e7fa38cb60b959dbc50f0145cdd7cfb31c618a7f
Keagan <sir.ellenberger@gmail.com>
2020-03-31 12:39:10 +0200

    Board list made

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -1,9 +1,10 @@
-﻿module Oware
+﻿// RULES: http://www.joansala.com/auale/rules/en/
+module Oware
 
 type StartingPosition =
     | South
     | North
-
+ 
 let getSeeds n board = failwith "Not implemented"
 
 let useHouse n board = failwith "Not implemented"
@@ -17,4 +18,14 @@ let gameState board = failwith "Not implemented"
 [<EntryPoint>]
 let main _ =
     printfn "Hello from F#!"
-    0 // return an integer exit code
+   // 0 // return an integer exit code
+    let board = [4; 4; 4; 4; 4; 4; 4; 4; 4; 4; 4; 4]
+    printfn "Holes: %A" board
+    0
+(*Thoughts: 
+3 Phases: Game states ? Collecting, Sowing, Capturing <- All in a turn
+
+
+
+
+*)
