fc3ba4e624a99a321903215b6d8c886fa4887c03
Power-Circuit <phirikhumbolane@gmail.com>
2020-05-17 14:56:11 +0200

    added incrementSeed and chooseHouse functions

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -36,7 +36,31 @@ let getSeeds n board =
    | 12 -> fS
    | _ -> failwith "Invalid Choice of hole"  // g17e4476: If user picks an invalid hole
 
+//Used in the useHouse function 
+//g19p6350
+let incrementSeed n (aN,bN,cN,dN,eN,fN,aS,bS,cS,dS,eS,fS) = 
+    //Used to increment preceeding houses
+    match n with 
+    //South side
+    |1 -> (aN+1,bN,cN,dN,eN,fN,aN',bN',cN',dN',eN',fN')
+    |2 -> (aN,bN+1,cN,dN,eN,fN,aN',bN',cN',dN',eN',fN')
+    |3 -> (aN,bN,cN+1,dN,eN,fN,aN',bN',cN',dN',eN',fN')
+    |4 -> (aN,bN,cN,dN+1,eN,fN,aN',bN',cN',dN',eN',fN')
+    |5 -> (aN,bN,cN,dN,eN+1,fN,aN',bN',cN',dN',eN',fN')
+    |6 -> (aN,bN,cN,dN,eN,fN+1,aN',bN',cN',dN',eN',fN')
+    //North side 
+    |7 -> (aS,bS,cS,dS,eS,fS,aS'+1,bS',cS',dS',eS',fS')
+    |8 -> (aS,bS,cS,dS,eS,fS,aS',bS'+1,cS',dS',eS',fS')
+    |9 -> (aS,bS,cS,dS,eS,fS,aS',bS',cS'+1,dS',eS',fS')
+    |10 -> (aS,bS,cS,dS,eS,fS,aS',bS',cS',dS'+1,eS',fS')
+    |11 -> (aS,bS,cS,dS,eS,fS,aS',bS',cS',dS',eS'+1,fS')
+    |12 -> (aS,bS,cS,dS,eS,fS,aS',bS',cS',dS',eS',fS'+1)
+    |_ -> failwith "{incrementSeed} out of range"
+
+
 let useHouse n board = failwith "Not implemented"
+
+
 
 let start position = 
    let hole = (4,4,4,4,4,4)
@@ -71,6 +95,30 @@ let gameState board =
                 |North -> "North's turn"
                 |South -> "South's turn"
 
+
+
+let chooseHouse n board = 
+    //g19p6350
+    //The player takes their turn given a house number 
+    //then the chosen house is set to 0
+    let (aN,bN,cN,dN,eN,fN) = board.PlayerA.houses
+    let (aS,bS,cS,dS,eS,fS) = board.PlayerB.houses
+    match n with
+    |1  -> {board with PlayerA = {board.PlayerA with houses = (0,bN,cN,dN,eN,fN)} }
+    |2  -> {board with PlayerA = {board.PlayerA with houses = (aN,0,cN,dN,eN,fN)} }
+    |3  -> {board with PlayerA = {board.PlayerA with houses = (aN,bN,0,dN,eN,fN)} } 
+    |4  -> {board with PlayerA = {board.PlayerA with houses = (aN,bN,cN,0,eN,fN)} }
+    |5  -> {board with PlayerA = {board.PlayerA with houses = (aN,bN,cN,dN,0,fN)} } 
+    |6  -> {board with PlayerA = {board.PlayerA with houses = (aN,bN,cN,dN,eN,0)} } 
+    |7  -> {board with PlayerB = {board.PlayerB with houses = (0,bS,cS,dS,eS,fS)} }
+    |8  -> {board with PlayerB = {board.PlayerB with houses = (aS,0,cS,dS,eS,fS)} }
+    |9  -> {board with PlayerB = {board.PlayerB with houses = (aS,bS,0,dS,eS,fS)} }
+    |10 -> {board with PlayerB = {board.PlayerB with houses = (aS,bS,cS,0,eS,fS)} } 
+    |11 -> {board with PlayerB = {board.PlayerB with houses = (aS,bS,cS,dS,0,fS)} }
+    |12 -> {board with PlayerB = {board.PlayerB with houses = (aS,bS,cS,dS,eS,0)} }
+    |_  -> failwith "{chooseHouse} house is not in 1 and 12 range."
+
+//Used in the useHouse function below
 // g17e4476: returns the nth Hole and the nr of seeds in the nth hole
 let returnHole n board = 
    let hole = getSeeds n board
@@ -84,6 +132,12 @@ let printHoles board =
       | 13 -> ()
       | _ -> printfn "%s" (returnHole count board);(print (count+1))
    print 1
+
+let nextPlayersTurn position = 
+    //Simple function that is used to alternate player turns.
+    match position with
+    | South -> North //this means that South (player one) just had their turn and now it is North's (player two's) turn.
+    | North -> South //this means that North (player two) just had their turn and now it is South's (player one's) turn
 
 
    // g17e4476: We can play the game. Impure input
