60a3152275553c91f5e4cd6389b03463dfb7cee6
Keagan <sir.ellenberger@gmail.com>
2020-05-18 14:29:55 +0200

    fixing notation

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -57,13 +57,13 @@ let incrementSeedCount n (aN,bN,cN,dN,eN,fN,aS,bS,cS,dS,eS,fS) =
 
 // g17e4476: The tuple set up as specified to return NorthScore (NS) and South Score (SS)
 let score board =
-   let southScore,northScore = board.PlayerA.score , board.PlayerB.score 
+   let southScore,northScore = board.PlayerB.score , board.PlayerA.score 
    southScore,northScore  
 // ^^^ Sets up the string of the score for north and south (NS, SS respectively)
 
 // g17e4476: returns the state of the game using the strings as specified
 let gameState board =
-   let a,b = score board // g17e4476: player a's score, player b's score (Ns and SS respectively)
+   let b,a = score board // g17e4476: player a's score, player b's score (Ns and SS respectively)
    match a > 24 with 
    |true -> "North won"
    |false -> 
@@ -110,17 +110,10 @@ let chosenHole n board =
     |10 -> {board with PlayerB = {board.PlayerB with holes = (aS,bS,cS,0,eS,fS)} } 
     |11 -> {board with PlayerB = {board.PlayerB with holes = (aS,bS,cS,dS,0,fS)} }
     |12 -> {board with PlayerB = {board.PlayerB with holes = (aS,bS,cS,dS,eS,0)} }
-    |_  -> failwith "{chooseHouse} house is not in 1 and 12 range."
-
-let nextPlayersTurn position = 
-    //Simple function that is used to alternate player turns.
-    match position with
-    | South -> North //this means that South (player A) just had their turn and now it is North's (player two's) turn.
-    | North -> South //this means that North (player B) just had their turn and now it is South's (player one's) turn
+    |_  -> failwith "{chooseHouse} house is not in 1 and 12 range."  
 
 let useHouse n board =
    let originalState = board //Need to protec the original state of the board if we make an invalid move
-
    let endHole =
       match (getSeeds n board) + n > 12 with  //If the sum goes above 
       | true -> (getSeeds n board) + n-12
@@ -133,41 +126,41 @@ let useHouse n board =
       match getSeeds n board with 
       | 0 -> board  //User has chosen an empty hole
       |_ ->
-         let (aN,bN,cN,dN,eN,fN),(aS,bS,cS,dS,eS,fS) = (chosenHole n board).PlayerA.holes,(chosenHole n board).PlayerB.holes 
-         let holesUpdated = (aN,bN,cN,dN,eN,fN,aS,bS,cS,dS,eS,fS)
-         let nrSeeds = getSeeds n board
-         
-         let rec sowingSeeds n seedsToBeSown holesUpdated startingHole = 
-             let n = match n with // Wrap around function. Each time we get to the end of the board we need to go back to 1
-                     | 13 -> 1 
-                     | _ -> n
-             match  seedsToBeSown with
-             | 0 -> holesUpdated
-             | _ -> match n = startingHole with 
-                      |false -> sowingSeeds (n+1) (seedsToBeSown-1) (incrementSeedCount n holesUpdated) startingHole 
-                      |_ -> sowingSeeds (n+1) seedsToBeSown holesUpdated startingHole // We need to skip over the startingHole
-         
-         let (aN,bN,cN,dN,eN,fN,aS,bS,cS,dS,eS,fS) =  sowingSeeds (n+1) nrSeeds holesUpdated n
-         //We need to update the board after distribution of seeds.
-         let pA = {board.PlayerA with holes = (aN,bN,cN,dN,eN,fN); score = board.PlayerA.score; nrSeeds = (aN+bN+cN+dN+eN+fN)} //Simply sum the contents of each hole
-         let pB = {board.PlayerB with holes = (aS,bS,cS,dS,eS,fS); score = board.PlayerB.score; nrSeeds = (aS+bS+cS+dS+eS+fS)} 
-         let board = {board with PlayerA = pA; PlayerB = pB; Turn = board.Turn}
-        
-         //Since we've sown the seeds we now need to update the scoreboard, similar to the way the board was updated.
-         let scoreboard = incrementScore endHole board.currentTurn board 
-         let pA = {board.PlayerA with holes = scoreboard.PlayerA.holes; score = scoreboard.PlayerA.score}
-         let pB = {board.PlayerB with holes = scoreboard.PlayerB.holes; score = scoreboard.PlayerB.score}
-         // The board itself needs to contain the new score  
-         let updatedBoard = {board with PlayerA = pA; PlayerB = pB}
-         //Pass the turn
-         let turn = 
-            match turn with
-            | North -> South
-            | South -> North
+      let (aN,bN,cN,dN,eN,fN),(aS,bS,cS,dS,eS,fS) = (chosenHole n board).PlayerA.holes,(chosenHole n board).PlayerB.holes 
+      let holesUpdated = (aN,bN,cN,dN,eN,fN,aS,bS,cS,dS,eS,fS)
+      let nrSeeds = getSeeds n board
+      
+      let rec sowingSeeds n seedsToBeSown holesUpdated startingHole = 
+          let n = match n with // Wrap around function. Each time we get to the end of the board we need to go back to 1
+                  | 13 -> 1 
+                  | _ -> n
+          match  seedsToBeSown with
+          | 0 -> holesUpdated
+          | _ -> match n = startingHole with 
+                   |false -> sowingSeeds (n+1) (seedsToBeSown-1) (incrementSeedCount n holesUpdated) startingHole 
+                   |_ -> sowingSeeds (n+1) seedsToBeSown holesUpdated startingHole // We need to skip over the startingHole
+      
+      let (aN,bN,cN,dN,eN,fN,aS,bS,cS,dS,eS,fS) =  sowingSeeds (n+1) nrSeeds holesUpdated n
+      //We need to update the board after distribution of seeds.
+      let pA = {board.PlayerA with holes = (aN,bN,cN,dN,eN,fN); score = board.PlayerA.score; nrSeeds = (aN+bN+cN+dN+eN+fN)} //Simply sum the contents of each hole
+      let pB = {board.PlayerB with holes = (aS,bS,cS,dS,eS,fS); score = board.PlayerB.score; nrSeeds = (aS+bS+cS+dS+eS+fS)} 
+      let board = {board with PlayerA = pA; PlayerB = pB; Turn = board.Turn}
+      
+      //Since we've sown the seeds we now need to update the scoreboard, similar to the way the board was updated.
+      let scoreboard = incrementScore endHole board.Turn board 
+      let pA = {board.PlayerA with holes = scoreboard.PlayerA.holes; score = scoreboard.PlayerA.score}
+      let pB = {board.PlayerB with holes = scoreboard.PlayerB.holes; score = scoreboard.PlayerB.score}
+      // The board itself needs to contain the new score  
+      let updatedBoard = {board with PlayerA = pA; PlayerB = pB}
+      //Pass the turn
+      let turn = 
+         match board.Turn with
+         | North -> South
+         | South -> North
 
-         match 1 with
-         | 0 -> originalState
-         | _ -> { updatedBoard with Turn = turn }
+      match 1 with
+      | 0 -> originalState
+      | _ -> { updatedBoard with Turn = turn }
 
 
 let returnScore board = 
@@ -220,3 +213,5 @@ let main _ =
    // g17e4476: Get's us into the game
     let beginning = beginGame (start North)
     0
+
+    
